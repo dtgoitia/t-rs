871a96cc42d3a846a8c4154736a97f64d4e0c9f0
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-11-05 13:31:17 +0000

    feat: do not hardcode user home directory

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+use std::env;
 use std::path::Path;
+use std::process;
 
 mod config;
 
 fn main() {
-    // TODO: how to do Path.expanduser()
-    let user_home = Path::new("/home/dtg");
+    let home = match env::var("HOME") {
+        Ok(value) => value,
+        Err(error) => return exit_with_error(error.to_string()),
+    };
 
-    let dotfiles_dir = user_home.join(".config/t");
+    let dotfiles_dir = Path::new(&home).join(".config/t");
 
     let config_path = dotfiles_dir.join("config.jsonc");
     let credentials_path = dotfiles_dir.join("credentials.jsonc");
+
     let config = config::load(&config_path, &credentials_path);
 
     /*
@@ -17,4 +22,9 @@ fn main() {
     */
     println!("{}", config.projects[0].name);
     return;
+}
+
+fn exit_with_error(message: String) {
+    println!("{}", message);
+    process::exit(-1);
 }
