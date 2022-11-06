44970778fc81dd39177939d58981b0e4167916c1
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-11-06 08:32:39 +0000

    feat: build dummy fuzzy-selector

--- src/toggl.rs
+++ src/toggl.rs
@@ -1,7 +1,20 @@
+use dialoguer::{console::Term, theme::ColorfulTheme, FuzzySelect};
+
 use crate::config::AppConfig;
 
 pub fn status(config: &AppConfig) {
     println!("Showing timer status! token {}", config.api_token.as_str());
+    let items = vec!["Item 1", "Item 2"];
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr())
+        .expect("TODO: handle this error");
+
+    match selection {
+        Some(index) => println!("User selected item: {}", items[index]),
+        None => println!("USer did not select anything"),
+    };
 }
 
 pub fn start(config: &AppConfig) {
